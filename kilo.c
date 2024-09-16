eb2181ea593cbb477c27c8473f6479ced481b183
Delwin Perera <perera@mm.tu-darmstadt.de>
2024-09-16 23:01:02 +0200

    Move cursor
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

--- kilo.c
+++ kilo.c
@@ -170,12 +170,38 @@ void editorRefreshScreen() {
 
 /*** input ***/
 
+void editorMoveCursor(char key) {
+    switch (key) {
+        case 'h':
+            E.cx--;
+            break;
+        case 'j':
+            E.cy++;
+            break;
+        case 'k':
+            E.cy--;
+            break;
+        case 'l':
+            E.cx++;
+            break;
+    }
+}
+
 void editorProcessKeypress() {
     char c = editorReadKey();
 
     switch (c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+        
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+            editorMoveCursor(c);
             break;
     }
 }
