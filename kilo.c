ff91c2e68975df93927d74a54db09d668cfd44f0
Delwin Perera <52405402+perdel@users.noreply.github.com>
2024-10-16 23:06:07 +0200

    Insert ordinary characters (#8)

--- kilo.c
+++ kilo.c
@@ -229,6 +229,26 @@ void editorAppendRow(const char *s, size_t len) {
     E.numrows++;
 }
 
+void editorRowInsertChar(erow *row, int at, char c) {
+    if (at < 0 || at > row->size) at = row->size;
+    row->chars = realloc(row->chars, row->size + 2);
+    memmove(row->chars + at + 1, row->chars + at, row->size - at + 1);
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+/*** row operations ***/
+
+void editorInsertChar(int c) {
+    if (E.cy == E.numrows) {
+        editorAppendRow("", 0);
+    }
+
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
 /*** file io ***/
 
 void editorOpen(char *filename) {
@@ -469,6 +489,10 @@ void editorProcessKeypress() {
         case ARROW_LEFT:
         case ARROW_RIGHT:
             editorMoveCursor(c);
+            break;
+        
+        default:
+            editorInsertChar(c);
             break;
     }
 }
