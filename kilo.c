42030ff13a1c30ccea28c639eb41e2c6692971df
Delwin Perera <perera@mm.tu-darmstadt.de>
2024-10-16 21:13:57 +0200

    Activate keypress insert
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

--- kilo.c
+++ kilo.c
@@ -490,6 +490,10 @@ void editorProcessKeypress() {
         case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
+        
+        default:
+            editorInsertChar(c);
+            break;
     }
 }
 /*** init ***/
