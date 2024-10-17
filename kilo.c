57c298ac086548ed7402ed9feacb41a9cf6c0ef8
Delwin Perera <perera@mm.tu-darmstadt.de>
2024-10-17 21:32:23 +0200

    Block special chars
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

--- kilo.c
+++ kilo.c
@@ -24,6 +24,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -453,6 +454,10 @@ void editorProcessKeypress() {
     int c = editorReadKey();
 
     switch (c) {
+        case '\r':
+            /* TODO */
+            break;
+
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
@@ -466,6 +471,12 @@ void editorProcessKeypress() {
         case END_KEY:
             if (E.cy < E.numrows)
                 E.cx = E.row[E.cy].size;
+            break;
+        
+        case BACKSPACE:
+        case CTRL_KEY('h'):
+        case DEL_KEY:
+            /* TODO*/
             break;
 
         case PAGE_UP:
@@ -491,6 +502,10 @@ void editorProcessKeypress() {
             editorMoveCursor(c);
             break;
         
+        case CTRL_KEY('l'):
+        case '\x1b':
+            break;
+
         default:
             editorInsertChar(c);
             break;
