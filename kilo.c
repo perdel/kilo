12e80e5457d04ab3d23765bcf983ea54246d500d
Delwin Perera <perera@mm.tu-darmstadt.de>
2024-10-24 23:24:40 +0200

    Map enter key
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

--- kilo.c
+++ kilo.c
@@ -572,7 +572,7 @@ void editorProcessKeypress() {
 
     switch (c) {
         case '\r':
-            /* TODO */
+            editorInsertNewline();
             break;
 
         case CTRL_KEY('q'):
