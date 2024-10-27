f12ff84dfc276e9b58b1e6e7f9a49b22c493873e
Delwin Perera <52405402+perdel@users.noreply.github.com>
2024-10-27 21:03:35 +0100

    Add basic search (#19)

--- kilo.c
+++ kilo.c
@@ -201,6 +201,19 @@ int editorRowCxToRx(erow *row, int cx) {
     return rx;
 }
 
+int editorRowRxToCx(erow *row, int rx) {
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; cx++) {
+        if (row->chars[cx] == '\t') 
+            cur_rx += (KILO_TAB_STOPS - 1) - (cur_rx % KILO_TAB_STOPS);
+        cur_rx++;
+
+        if (cur_rx > rx) return cx;
+    }
+    return cx;
+}
+
 void editorUpdateRow(erow *row) {
     int tabs = 0;
     int j;
@@ -394,6 +407,25 @@ void editorSave() {
     editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
+/*** find ***/
+
+void editorFind() {
+    char *query = editorPrompt("Search (ESC to cancel): %s");
+    if (query == NULL) return;
+
+    int i;
+    for (i = 0; i < E.numrows; i++) {
+        erow *row = &E.row[i];
+        char *match = strstr(row->chars, query);
+        if (match) {
+            E.cy = i;
+            E.cx = editorRowRxToCx(row, match - row->render);
+            break;
+        }
+    }
+
+    free(query);
+}
 /*** append buffer ***/
 
 struct abuf {
@@ -641,6 +673,10 @@ void editorProcessKeypress() {
                 E.cx = E.row[E.cy].size;
             break;
         
+        case CTRL_KEY('f'):
+            editorFind();
+            break;
+        
         case BACKSPACE:
         case CTRL_KEY('h'):
         case DEL_KEY:
@@ -708,7 +744,7 @@ int main(int argc, char *argv[]) {
         editorOpen(argv[1]);
     } 
 
-    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
     while (1) {
         editorRefreshScreen();
